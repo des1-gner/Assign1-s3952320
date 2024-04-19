efd0a5a71dd01f6d4cd9066d5eabd235b5940f36
s3952320 <s3952320@student.rmit.edu.au>
2024-04-19 13:29:54 +1000

    timed also commented

--- dataGen/timedClasses/adjListGraph.py
+++ dataGen/timedClasses/adjListGraph.py
@@ -2,18 +2,28 @@
 # Please COMPLETE the IMPLEMENTATION of this class.
 # Adjacent list implementation.
 #
-# __author__ = 'Jeffrey Chan', Oisin Aeonn s3952320
+# __author__ = 'Jeffrey Chan', 'Oisin Aeonn s3952320'
 # __copyright__ = 'Copyright 2024, RMIT University'
 # ------------------------------------------------------------------------
 
 from typing import List
+
 from maze.util import Coordinates
+
 from maze.graph import Graph
+
 import timeit
 import atexit
 
 class AdjListGraph:
+
+    """
+    Represents an undirected graph. Please complete the implementations of each method. See the documentation for the parent class
+    to see what each of the overriden methods are meant to do.
+    """
+
     function_times = {
+    
         "__init__": [],
         "addVertex": [],
         "addVertices": [],
@@ -24,8 +34,11 @@ class AdjListGraph:
         "hasEdge": [],
         "getWallStatus": [],
         "neighbours": []
+    
     }
+    
     function_calls = {
+    
         "__init__": 0,
         "addVertex": 0,
         "addVertices": 0,
@@ -36,12 +49,24 @@ class AdjListGraph:
         "hasEdge": 0,
         "getWallStatus": 0,
         "neighbours": 0
+    
     }
 
+    # Initialises an empty Graph
+
     def __init__(self):
+    
         start_time = timeit.default_timer()
 
-        # Dictionary to store adjacency list, key is a vertex and value is the list of neighbours.
+        ### Implement me! ###
+
+        """
+        Dictionary to store the Adjacency List where: 
+        Key is the Vertex, 
+                & 
+        Value is the List of Neighbours
+        """
+        
         self.adjList = {}
 
         end_time = timeit.default_timer()
@@ -49,11 +74,16 @@ class AdjListGraph:
         AdjListGraph.function_times["__init__"].append(execution_time)
         AdjListGraph.function_calls["__init__"] += 1
 
-    def addVertex(self, label: Coordinates):
-        start_time = timeit.default_timer()
+    # Adds a Vertex to the Graph if it does not exist
 
-        # Add a vertex to the graph if it does not exist
+    def addVertex(self, label:Coordinates):
+    
+        start_time = timeit.default_timer()
+        
+        ### Implement me! ###
+
         if label not in self.adjList:
+            
             self.adjList[label] = []
 
         end_time = timeit.default_timer()
@@ -61,11 +91,16 @@ class AdjListGraph:
         AdjListGraph.function_times["addVertex"].append(execution_time)
         AdjListGraph.function_calls["addVertex"] += 1
 
-    def addVertices(self, vertLabels: List[Coordinates]):
-        start_time = timeit.default_timer()
+    # Adds multiple Vertices with the given coordinates to the Graph
 
-        # Add multiple vertices to the graph
+    def addVertices(self, vertLabels: List[Coordinates]):
+    
+        start_time = timeit.default_timer()
+        
+        ### Implement me! ###
+
         for label in vertLabels:
+            
             self.addVertex(label)
 
         end_time = timeit.default_timer()
@@ -73,135 +108,259 @@ class AdjListGraph:
         AdjListGraph.function_times["addVertices"].append(execution_time)
         AdjListGraph.function_calls["addVertices"] += 1
 
-    def addEdge(self, vert1: Coordinates, vert2: Coordinates, addWall: bool = False) -> bool:
-        start_time = timeit.default_timer()
+    # Adds an undirected Edge between coordinates
 
-        # Adds an undirected edge from vert1 to vert2 (and vice versa)
-        if vert1 in self.adjList and vert2 in self.adjList:
+    def addEdge(self, vert1: Coordinates, vert2: Coordinates, addWall: bool = False) -> bool:
+    
+        start_time = timeit.default_timer()
+        
+        ### Implement me! ###
+
+        # Adds an undirected edge from coordinates
+
+        if vert1 in self.adjList and vert2 in self.adjList: # if addWall() is True, the Edge is marked as a Wall
+            
             self.adjList[vert1].append((vert2, addWall))
             self.adjList[vert2].append((vert1, addWall))
+            
+            # remember to return booleans
+
+            # True if Edge is added successfully 
+            
             result = True
+                
+        # Else False
+        
         else:
+    
             result = False
 
         end_time = timeit.default_timer()
         execution_time = end_time - start_time
         AdjListGraph.function_times["addEdge"].append(execution_time)
         AdjListGraph.function_calls["addEdge"] += 1
+    
         return result
 
-    def updateWall(self, vert1: Coordinates, vert2: Coordinates, wallStatus: bool) -> bool:
-        start_time = timeit.default_timer()
+    # Update the Wall status of the Edge between coordinates
 
-        # Updates the wall status of an edge
-        if vert1 in self.adjList and vert2 in self.adjList:
+    def updateWall(self, vert1: Coordinates, vert2: Coordinates, wallStatus: bool) -> bool:
+    
+        start_time = timeit.default_timer()
+        
+        ### Implement me! ###
+
+        # Updates the Wall status of an Edge
+        
+        if vert1 in self.adjList and vert2 in self.adjList: # if addWall() is True, the Edge is a Wall
+        
             for i, (vertex, isWall) in enumerate(self.adjList[vert1]):
+        
                 if vertex == vert2:
+        
                     self.adjList[vert1][i] = (vertex, wallStatus)
+        
             for i, (vertex, isWall) in enumerate(self.adjList[vert2]):
+        
                 if vertex == vert1:
+        
                     self.adjList[vert2][i] = (vertex, wallStatus)
+
+            # remember to return booleans
+
+            # True if Edge is added
+
             result = True
+        
+        # Else False
         else:
+    
             result = False
 
         end_time = timeit.default_timer()
         execution_time = end_time - start_time
         AdjListGraph.function_times["updateWall"].append(execution_time)
         AdjListGraph.function_calls["updateWall"] += 1
+    
         return result
 
-    def removeEdge(self, vert1: Coordinates, vert2: Coordinates) -> bool:
-        start_time = timeit.default_timer()
+    # Removes an undirected Edge between coordinates
 
-        # Removes the undirected edge from vert1 to vert2 (and vice versa)
-        if vert1 in self.adjList and vert2 in self.adjList:
+    def removeEdge(self, vert1: Coordinates, vert2: Coordinates) -> bool:
+    
+        start_time = timeit.default_timer()
+        
+        ### Implement me! ###
+
+        # Removes the undirected Edge from coordinates
+
+        if vert1 in self.adjList and vert2 in self.adjList: 
+        
             self.adjList[vert1] = [neighbour for neighbour in self.adjList[vert1] if neighbour[0] != vert2]
             self.adjList[vert2] = [neighbour for neighbour in self.adjList[vert2] if neighbour[0] != vert1]
+
+            # remember to return booleans
+
+            # True if Vertex exists
+        
             result = True
-        else:
+        
+        # Else False
+    
+        else: 
+    
             result = False
 
         end_time = timeit.default_timer()
         execution_time = end_time - start_time
         AdjListGraph.function_times["removeEdge"].append(execution_time)
         AdjListGraph.function_calls["removeEdge"] += 1
+    
         return result
 
-    def hasVertex(self, label: Coordinates) -> bool:
-        start_time = timeit.default_timer()
+    # Checks if the Vertex of the coordinates exists in the Graph
 
-        # Checks if a vertex exists in the graph
+    def hasVertex(self, label: Coordinates) -> bool:
+    
+        start_time = timeit.default_timer()
+        
+        ### Implement me! ###
+
+        # Checks if a Vertex exists in the Graph
+
+        # remember to return booleans
+
+        # Returns True if the Vertex exists, else False
+
         result = label in self.adjList
 
         end_time = timeit.default_timer()
         execution_time = end_time - start_time
         AdjListGraph.function_times["hasVertex"].append(execution_time)
         AdjListGraph.function_calls["hasVertex"] += 1
+    
         return result
 
-    def hasEdge(self, vert1: Coordinates, vert2: Coordinates) -> bool:
-        start_time = timeit.default_timer()
+    # Checks if an Edge exists between the coordinates
 
-        # Checks if an edge exists between vert1 and vert2
+    def hasEdge(self, vert1: Coordinates, vert2: Coordinates) -> bool:
+    
+        start_time = timeit.default_timer()
+        
+        ### Implement me! ###
+
         if vert1 in self.adjList:
+
+            # remember to return booleans
+
+            # True if Edge exists
+        
             result = any(neighbour[0] == vert2 for neighbour in self.adjList[vert1])
+        
+        # Else False
+    
         else:
+    
             result = False
 
         end_time = timeit.default_timer()
         execution_time = end_time - start_time
         AdjListGraph.function_times["hasEdge"].append(execution_time)
         AdjListGraph.function_calls["hasEdge"] += 1
+    
         return result
 
-    def getWallStatus(self, vert1: Coordinates, vert2: Coordinates) -> bool:
-        start_time = timeit.default_timer()
+    # Check if the Edge between coordinates is marked as a Wall
 
-        # Returns the wall status between vert1 and vert2
+    def getWallStatus(self, vert1: Coordinates, vert2: Coordinates) -> bool:
+    
+        start_time = timeit.default_timer()
+        
+        ### Implement me! ###
+
+        # Returns the Wall status between coordinates
+
         if vert1 in self.adjList:
+        
             for neighbour, isWall in self.adjList[vert1]:
+        
                 if neighbour == vert2:
+
+                    # remember to return booleans
+
+                    # True if the Edge is a Wall
+        
                     result = isWall
+    
                     break
             else:
+    
                 result = False
+    
         else:
+    
             result = False
 
         end_time = timeit.default_timer()
         execution_time = end_time - start_time
         AdjListGraph.function_times["getWallStatus"].append(execution_time)
         AdjListGraph.function_calls["getWallStatus"] += 1
+    
         return result
 
-    def neighbours(self, label: Coordinates) -> List[Coordinates]:
-        start_time = timeit.default_timer()
+    # Returns a List of coordinates of the neighbouring Vertices
 
-        # Returns a list of neighbours for the given vertex
+    def neighbours(self, label: Coordinates) -> List[Coordinates]:
+    
+        start_time = timeit.default_timer()
+        
+        ### Implement me! ###
+        
+        # Returns a List of Neighbours for the given Vertex
+
         if label in self.adjList:
+
+            # remember to return list of coordinates
+
+            # Returns List if there are neighbours
+        
             result = [neighbour[0] for neighbour in self.adjList[label]]
+        
+        # Else empty List
+    
         else:
+    
             result = []
 
         end_time = timeit.default_timer()
         execution_time = end_time - start_time
         AdjListGraph.function_times["neighbours"].append(execution_time)
         AdjListGraph.function_calls["neighbours"] += 1
+    
         return result
 
 def print_average_times():
+    
     print("Average execution times:")
+    
     for function_name, times in AdjListGraph.function_times.items():
+    
         if len(times) > 0:
+    
             average_time = sum(times) / len(times)
+    
             print(f"{function_name}: {average_time:.6f} seconds")
+    
         else:
+    
             print(f"{function_name}: Not called")
 
 def print_function_calls():
+    
     print("Function call counts:")
+   
     for function_name, count in AdjListGraph.function_calls.items():
+   
         print(f"{function_name}: {count} calls")
 
 atexit.register(print_average_times)
--- dataGen/timedClasses/adjMatGraph.py
+++ dataGen/timedClasses/adjMatGraph.py
@@ -2,18 +2,29 @@
 # Please COMPLETE the IMPLEMENTATION of this class.
 # Adjacent matrix implementation.
 #
-# __author__ = 'Jeffrey Chan', Oisin Aeonn s3952320
+# __author__ = 'Jeffrey Chan', 'Oisin Aeonn s3952320'
 # __copyright__ = 'Copyright 2024, RMIT University'
 # ------------------------------------------------------------------------
 
 from typing import List, Dict
+
 from maze.util import Coordinates
+
 from maze.graph import Graph
+
 import timeit
+
 import atexit
 
 class AdjMatGraph:
+
+    """
+    Represents an undirected graph. Please complete the implementations of each method. See the documentation for the parent class
+    to see what each of the overriden methods are meant to do.
+    """
+
     function_times = {
+
         "__init__": [],
         "addVertex": [],
         "addVertices": [],
@@ -24,8 +35,10 @@ class AdjMatGraph:
         "hasEdge": [],
         "getWallStatus": [],
         "neighbours": []
+
     }
     function_calls = {
+
         "__init__": 0,
         "addVertex": 0,
         "addVertices": 0,
@@ -36,179 +49,322 @@ class AdjMatGraph:
         "hasEdge": 0,
         "getWallStatus": 0,
         "neighbours": 0
+
     }
 
+    # Initialises an empty Graph
+
     def __init__(self):
+
         start_time = timeit.default_timer()
-
-        self.adjMatrix = []  # Adjacency matrix to store edges
-        self.vertices = []  # List to store vertices
-        self.vertex_indices = {}  # Dictionary to store vertex-index mappings
-
+        
+        ### Implement me! ###
+        
+        self.adjMatrix = [] # List to store Edges of the Adjacency Matrix
+        self.vertices = [] # List to store Vertices of the Adjacency Matrix
+        self.vertex_indices = {} # Dictionary to store Vertex-Index Mappings
+        
         end_time = timeit.default_timer()
         execution_time = end_time - start_time
         AdjMatGraph.function_times["__init__"].append(execution_time)
         AdjMatGraph.function_calls["__init__"] += 1
 
-    def addVertex(self, label):
+    # Adds a Vertex with the given label to the Graph
+
+    def addVertex(self, label:Coordinates):
+
         start_time = timeit.default_timer()
+        
+        ### Implement me! ###
 
         if label not in self.vertices:
+        
             self.vertices.append(label)
             self.vertex_indices[label] = len(self.vertices) - 1
 
-            # Expand the adjacency matrix for the new vertex
+            # Expand the Adjacency Matrix for the new Vertex
+        
             for row in self.adjMatrix:
+        
                 row.append(0)
+        
             self.adjMatrix.append([0] * len(self.vertices))
-
+            
         end_time = timeit.default_timer()
         execution_time = end_time - start_time
         AdjMatGraph.function_times["addVertex"].append(execution_time)
         AdjMatGraph.function_calls["addVertex"] += 1
 
-    def addVertices(self, vertLabels):
+    # Adds multiple Vertices with the given labels to the Graph
+
+    def addVertices(self, vertLabels:List[Coordinates]):
+
         start_time = timeit.default_timer()
 
+        ### Implement me! ###
+        
         for label in vertLabels:
+        
             self.addVertex(label)
-
+            
         end_time = timeit.default_timer()
         execution_time = end_time - start_time
         AdjMatGraph.function_times["addVertices"].append(execution_time)
         AdjMatGraph.function_calls["addVertices"] += 1
 
-    def addEdge(self, vert1, vert2, addWall=False):
+    # Adds an Edge between the Vertices 
+
+    def addEdge(self, vert1:Coordinates, vert2:Coordinates, addWall:bool=False)->bool:
+
         start_time = timeit.default_timer()
 
+        ### Implement me! ###
+
         if vert1 in self.vertex_indices and vert2 in self.vertex_indices:
+        
             index1 = self.vertex_indices[vert1]
             index2 = self.vertex_indices[vert2]
-            # Use 1 for edge, 2 to represent wall
+
+            # 1 represents an Edge, 2 represents a Wall
+        
             self.adjMatrix[index1][index2] = 2 if addWall else 1
             self.adjMatrix[index2][index1] = 2 if addWall else 1
-            result = True
-        else:
-            result = False
 
+            # remember to return booleans
+
+            # True if Edge is added successfully e.g. addWall() is True
+        
+            result = True
+        
+        # Else False
+        
+        else:
+
+            result = False
+            
         end_time = timeit.default_timer()
         execution_time = end_time - start_time
         AdjMatGraph.function_times["addEdge"].append(execution_time)
         AdjMatGraph.function_calls["addEdge"] += 1
+        
         return result
+    
+    # Updates the Wall status of the Edge between two coordinates
 
-    def updateWall(self, vert1, vert2, wallStatus):
+    def updateWall(self, vert1:Coordinates, vert2:Coordinates, wallStatus:bool)->bool:
+
         start_time = timeit.default_timer()
+        
+        ### Implement me! ###
 
         if vert1 in self.vertex_indices and vert2 in self.vertex_indices:
+        
             index1 = self.vertex_indices[vert1]
             index2 = self.vertex_indices[vert2]
-            if self.adjMatrix[index1][index2] != 0:  # Edge exists
+
+            # Edge exists
+
+            if self.adjMatrix[index1][index2] != 0: 
+
+                # 1 represents an Edge, 2 represents a Wall
+            
                 self.adjMatrix[index1][index2] = 2 if wallStatus else 1
                 self.adjMatrix[index2][index1] = 2 if wallStatus else 1
-                result = True
-            else:
-                result = False
-        else:
-            result = False
 
+                # remember to return booleans
+            
+                result = True
+            
+            else:
+
+                result = False
+        
+        else:
+
+            result = False
+            
         end_time = timeit.default_timer()
         execution_time = end_time - start_time
         AdjMatGraph.function_times["updateWall"].append(execution_time)
         AdjMatGraph.function_calls["updateWall"] += 1
+        
         return result
+    
+    # Removes the Edge between coordinates
 
-    def removeEdge(self, vert1, vert2):
+    def removeEdge(self, vert1:Coordinates, vert2:Coordinates)->bool:
+
         start_time = timeit.default_timer()
+        
+        ### Implement me! ###
 
         if vert1 in self.vertex_indices and vert2 in self.vertex_indices:
+        
             index1 = self.vertex_indices[vert1]
             index2 = self.vertex_indices[vert2]
-            if self.adjMatrix[index1][index2] != 0:  # Edge exists
+
+            # Edge exists
+
+            if self.adjMatrix[index1][index2] != 0:  
+        
                 self.adjMatrix[index1][index2] = 0
                 self.adjMatrix[index2][index1] = 0
-                result = True
-            else:
-                result = False
-        else:
-            result = False
 
+                # remember to return booleans
+
+                # True if Edge is removed
+        
+                result = True
+            
+            else:
+
+                result = False
+        
+        # Else False
+        
+        else:
+
+            result = False
+            
         end_time = timeit.default_timer()
         execution_time = end_time - start_time
         AdjMatGraph.function_times["removeEdge"].append(execution_time)
         AdjMatGraph.function_calls["removeEdge"] += 1
+        
         return result
+    
+    # Checks if the Vertex label exists in the Graph
 
-    def hasVertex(self, label):
+    def hasVertex(self, label:Coordinates)->bool:
+
         start_time = timeit.default_timer()
+        
+        ### Implement me! ###
+
+        # Returns True if Vertex exists, else False
 
         result = label in self.vertex_indices
-
+        
         end_time = timeit.default_timer()
         execution_time = end_time - start_time
         AdjMatGraph.function_times["hasVertex"].append(execution_time)
         AdjMatGraph.function_calls["hasVertex"] += 1
+        
         return result
+    
+    # Checks if an Edge exists between two Vertices
 
-    def hasEdge(self, vert1, vert2):
+    def hasEdge(self, vert1:Coordinates, vert2:Coordinates)->bool:
+
         start_time = timeit.default_timer()
+        
+        ### Implement me! ###
 
         if vert1 in self.vertex_indices and vert2 in self.vertex_indices:
+        
             index1 = self.vertex_indices[vert1]
             index2 = self.vertex_indices[vert2]
-            result = self.adjMatrix[index1][index2] != 0
-        else:
-            result = False
 
+            # remember to return booleans
+
+            # Returns True if Edge exists
+        
+            result = self.adjMatrix[index1][index2] != 0
+        
+        # Else False
+        
+        else:
+
+            result = False
+            
         end_time = timeit.default_timer()
         execution_time = end_time - start_time
         AdjMatGraph.function_times["hasEdge"].append(execution_time)
         AdjMatGraph.function_calls["hasEdge"] += 1
+        
         return result
+    
+    # Checks if the Edge between coordinates is a Wall
 
-    def getWallStatus(self, vert1, vert2):
+    def getWallStatus(self, vert1:Coordinates, vert2:Coordinates)->bool:
+
         start_time = timeit.default_timer()
+        
+        ### Implement me! ###
 
         if vert1 in self.vertex_indices and vert2 in self.vertex_indices:
+        
             index1 = self.vertex_indices[vert1]
             index2 = self.vertex_indices[vert2]
-            result = self.adjMatrix[index1][index2] == 2
-        else:
-            result = False
 
+            # remember to return booleans
+
+            # Returns True if the Edge is a Wall
+        
+            result = self.adjMatrix[index1][index2] == 2
+        
+        # Else False
+        
+        else:
+
+            result = False
+            
         end_time = timeit.default_timer()
         execution_time = end_time - start_time
         AdjMatGraph.function_times["getWallStatus"].append(execution_time)
         AdjMatGraph.function_calls["getWallStatus"] += 1
+        
         return result
+    
+    # Returns a List of coordinates representing the neighbouring Vertices of the label
 
-    def neighbours(self, label):
+    def neighbours(self, label:Coordinates)->List[Coordinates]:
+
         start_time = timeit.default_timer()
+        
+        ### Implement me! ###
 
         if label not in self.vertex_indices:
+        
             result = []
-        else:
-            index = self.vertex_indices[label]
-            result = [self.vertices[i] for i, val in enumerate(self.adjMatrix[index]) if val != 0]
 
+        else:
+
+            index = self.vertex_indices[label]
+
+            # remember to return list of coordinates
+        
+            result = [self.vertices[i] for i, val in enumerate(self.adjMatrix[index]) if val != 0]
+            
         end_time = timeit.default_timer()
         execution_time = end_time - start_time
         AdjMatGraph.function_times["neighbours"].append(execution_time)
         AdjMatGraph.function_calls["neighbours"] += 1
+        
         return result
 
 def print_average_times():
+
     print("Average execution times:")
+
     for function_name, times in AdjMatGraph.function_times.items():
+
         if len(times) > 0:
+
             average_time = sum(times) / len(times)
+
             print(f"{function_name}: {average_time:.6f} seconds")
+
         else:
+
             print(f"{function_name}: Not called")
 
 def print_function_calls():
+
     print("Function call counts:")
+
     for function_name, count in AdjMatGraph.function_calls.items():
+
         print(f"{function_name}: {count} calls")
 
 atexit.register(print_average_times)

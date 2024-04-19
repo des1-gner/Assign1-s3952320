6f82919fb104f09cef57cdfaab16b6e9d2cbd3b7
s3952320 <s3952320@student.rmit.edu.au>
2024-04-19 12:02:50 +1000

    commented code

--- maze/adjListGraph.py
+++ maze/adjListGraph.py
@@ -6,76 +6,133 @@
 # __copyright__ = 'Copyright 2024, RMIT University'
 # ------------------------------------------------------------------------
 
-
 from typing import List
 
 from maze.util import Coordinates
+
 from maze.graph import Graph
 
-
 class AdjListGraph:
+
+
+
     def __init__(self):
+
+        ### Implement me! ###
+
         # Dictionary to store adjacency list, key is a vertex and value is the list of neighbours.
         self.adjList = {}
 
     def addVertex(self, label: Coordinates):
+        
+        ### Implement me! ###
+
         # Add a vertex to the graph if it does not exist
         if label not in self.adjList:
+            
             self.adjList[label] = []
 
     def addVertices(self, vertLabels: List[Coordinates]):
+        
+        ### Implement me! ###
+
         # Add multiple vertices to the graph
         for label in vertLabels:
+            
             self.addVertex(label)
 
     def addEdge(self, vert1: Coordinates, vert2: Coordinates, addWall: bool = False) -> bool:
+        
+        ### Implement me! ###
+
         # Adds an undirected edge from vert1 to vert2 (and vice versa)
         if vert1 in self.adjList and vert2 in self.adjList:
+            
             self.adjList[vert1].append((vert2, addWall))
             self.adjList[vert2].append((vert1, addWall))
+            
             return True
+        
         return False
 
     def updateWall(self, vert1: Coordinates, vert2: Coordinates, wallStatus: bool) -> bool:
+        
+        ### Implement me! ###
+
         # Updates the wall status of an edge
+        
         if vert1 in self.adjList and vert2 in self.adjList:
+        
             for i, (vertex, isWall) in enumerate(self.adjList[vert1]):
+        
                 if vertex == vert2:
+        
                     self.adjList[vert1][i] = (vertex, wallStatus)
+        
             for i, (vertex, isWall) in enumerate(self.adjList[vert2]):
+        
                 if vertex == vert1:
+        
                     self.adjList[vert2][i] = (vertex, wallStatus)
+        
             return True
+        
         return False
 
     def removeEdge(self, vert1: Coordinates, vert2: Coordinates) -> bool:
+        
+        ### Implement me! ###
+
         # Removes the undirected edge from vert1 to vert2 (and vice versa)
         if vert1 in self.adjList and vert2 in self.adjList:
+        
             self.adjList[vert1] = [neighbour for neighbour in self.adjList[vert1] if neighbour[0] != vert2]
             self.adjList[vert2] = [neighbour for neighbour in self.adjList[vert2] if neighbour[0] != vert1]
+        
             return True
+        
         return False
 
     def hasVertex(self, label: Coordinates) -> bool:
+        
+        ### Implement me! ###
+
         # Checks if a vertex exists in the graph
         return label in self.adjList
 
     def hasEdge(self, vert1: Coordinates, vert2: Coordinates) -> bool:
+        
+        ### Implement me! ###
+
         # Checks if an edge exists between vert1 and vert2
         if vert1 in self.adjList:
+        
             return any(neighbour[0] == vert2 for neighbour in self.adjList[vert1])
+        
         return False
 
     def getWallStatus(self, vert1: Coordinates, vert2: Coordinates) -> bool:
+        
+        ### Implement me! ###
+
         # Returns the wall status between vert1 and vert2
         if vert1 in self.adjList:
+        
             for neighbour, isWall in self.adjList[vert1]:
+        
                 if neighbour == vert2:
+        
                     return isWall
+        
         return False
 
     def neighbours(self, label: Coordinates) -> List[Coordinates]:
+        
+        ### Implement me! ###
+        
         # Returns a list of neighbours for the given vertex
         if label in self.adjList:
+        
             return [neighbour[0] for neighbour in self.adjList[label]]
+        
         return []
--- maze/adjMatGraph.py
+++ maze/adjMatGraph.py
@@ -2,10 +2,9 @@
 # Please COMPLETE the IMPLEMENTATION of this class.
 # Adjacent matrix implementation.
 #
-# __author__ = 'Jeffrey Chan', Oisin Aeonn s3952320
+# __author__ = 'Jeffrey Chan', 'Oisin Aeonn s3952320'
 # __copyright__ = 'Copyright 2024, RMIT University'
 # ------------------------------------------------------------------------
-
 
 from typing import List, Dict
 
@@ -14,78 +13,151 @@ from maze.util import Coordinates
 from maze.graph import Graph
 
 class AdjMatGraph:
+    
+    """
+    Represents an undirected graph.  Please complete the implementations of each method.  See the documentation for the parent class
+    to see what each of the overriden methods are meant to do.
+    """
+    
     def __init__(self):
-        self.adjMatrix = []  # Adjacency matrix to store edges
-        self.vertices = []  # List to store vertices
-        self.vertex_indices = {}  # Dictionary to store vertex-index mappings
+        
+        ### Implement me! ###
+        
+        self.adjMatrix = [] # List to store Edges of the Adjacency Matrix
+        self.vertices = [] # List to store Vertices of the Adjacency Matrix
+        self.vertex_indices = {} # Dictionary to store Vertex-Index Mappings
 
-    def addVertex(self, label):
+    def addVertex(self, label:Coordinates):
+        
+        ### Implement me! ###
+
         if label not in self.vertices:
+        
             self.vertices.append(label)
             self.vertex_indices[label] = len(self.vertices) - 1
 
             # Expand the adjacency matrix for the new vertex
+        
             for row in self.adjMatrix:
+        
                 row.append(0)
+        
             self.adjMatrix.append([0] * len(self.vertices))
 
-    def addVertices(self, vertLabels):
+    def addVertices(self, vertLabels:List[Coordinates]):
+
+        ### Implement me! ###
+        
         for label in vertLabels:
+        
             self.addVertex(label)
 
     def addEdge(self, vert1, vert2, addWall=False):
+
+        ### Implement me! ###
+
         if vert1 in self.vertex_indices and vert2 in self.vertex_indices:
+        
             index1 = self.vertex_indices[vert1]
             index2 = self.vertex_indices[vert2]
 
             # Use 1 for edge, 2 to represent wall
+        
             self.adjMatrix[index1][index2] = 2 if addWall else 1
             self.adjMatrix[index2][index1] = 2 if addWall else 1
+
+            # remember to return booleans
+        
             return True
+        
         return False
 
     def updateWall(self, vert1, vert2, wallStatus):
+        
+        ### Implement me! ###
+
         if vert1 in self.vertex_indices and vert2 in self.vertex_indices:
+        
             index1 = self.vertex_indices[vert1]
             index2 = self.vertex_indices[vert2]
 
             if self.adjMatrix[index1][index2] != 0:  # Edge exists
+            
                 self.adjMatrix[index1][index2] = 2 if wallStatus else 1
                 self.adjMatrix[index2][index1] = 2 if wallStatus else 1
+
+                # remember to return booleans
+            
                 return True
+        
         return False
 
     def removeEdge(self, vert1, vert2):
+        
+        ### Implement me! ###
+
         if vert1 in self.vertex_indices and vert2 in self.vertex_indices:
+        
             index1 = self.vertex_indices[vert1]
             index2 = self.vertex_indices[vert2]
 
             if self.adjMatrix[index1][index2] != 0:  # Edge exists
+        
                 self.adjMatrix[index1][index2] = 0
                 self.adjMatrix[index2][index1] = 0
+
+                # remember to return booleans
+        
                 return True
+        
         return False
 
     def hasVertex(self, label):
+        
+        ### Implement me! ###
+
         return label in self.vertex_indices
 
     def hasEdge(self, vert1, vert2):
+        
+        ### Implement me! ###
+
         if vert1 in self.vertex_indices and vert2 in self.vertex_indices:
+        
             index1 = self.vertex_indices[vert1]
             index2 = self.vertex_indices[vert2]
+
+            # remember to return booleans
+        
             return self.adjMatrix[index1][index2] != 0
+        
         return False
 
     def getWallStatus(self, vert1, vert2):
+        
+        ### Implement me! ###
+
         if vert1 in self.vertex_indices and vert2 in self.vertex_indices:
+        
             index1 = self.vertex_indices[vert1]
             index2 = self.vertex_indices[vert2]
+
+            # remember to return booleans
+        
             return self.adjMatrix[index1][index2] == 2
+        
         return False
 
     def neighbours(self, label):
+        
+        ### Implement me! ###
+
         if label not in self.vertex_indices:
+        
             return []
 
         index = self.vertex_indices[label]
+
+        # remember to return list of coordinates
+        
         return [self.vertices[i] for i, val in enumerate(self.adjMatrix[index]) if val != 0]
